871561fcfeeaab42f0d46412fb089723fbab94d4
Karthikeyan1993 <karthik.infotec99@gmail.com>
2017-04-25 10:07:26 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var radius;
+radius=5;
+
+var areaOfCircle=Calc(radius);
+//Calc function will return area of circle
+function Calc(radius) {
+  // body...
+return ((radius*radius)*Math.PI);
+}
+
+console.log(areaOfCircle);
